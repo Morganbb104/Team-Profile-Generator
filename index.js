6365944054feec071f39b4baf31043d4e8b552dd
CHENG CHIEN <hcc780921@gmail.com>
2022-02-28 20:54:36 -0800

    html template created

--- index.js
+++ index.js
@@ -1,154 +1,139 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown;
+const Manager = require("./tests/Manager");
+const Engineer = require("./tests/Engineer");
+const Intern = require("./tests/Intern");
+const inquirer = require("inquirer");
+const generateHTML = require('./rsc/template');
+const fs = require("fs")
 
-const questions=[
-    {//Project name
-        type: "text",
-        message: "what is your project Name?",
-        name: "name",
-        default:'Cheng',
-      },
-      {//Description
-        type: "input",
-        message: "Please leave a description of your project.",
-        name: "Description",
-        default:' A node.js application that uses user input from inquirer to populate a README.md file for user repository.'
+class Prompts {
+    constructor() {
+        this.employees = [];
+    }
 
-      },
-      {//Installation
-        type: "input",
-        message: "install",
-        name: "npm",
-        default:'project install instructions'
+    promptUser() {
+        inquirer
+            .prompt([
+                {
+                    type: 'text',
+                    name: 'name',
+                    message: 'What is your managers name?'
+                },
+                {
+                    type: 'text',
+                    name: 'id',
+                    message: 'What is your managers id?'
+                },
+                {
+                    type: 'text',
+                    name: 'email',
+                    message: 'What is your managers email?'
+                },
+                {
+                    type: 'text',
+                    name: 'officeNumber',
+                    message: 'What is your managers office number?'
+                }
+            ])
+            //destructure name from the prompt object
+            .then(({ name, id, email, officeNumber }) => {
+                this.employees.push(new Manager(name, id, email, officeNumber));
 
-      },{//usage
-        type: "input",
-        message: "what is this program used for?",
-        name: "use",
-        default:'usage'
-
-      },{// contribution guidelines
-        type: "input",
-        message: "Please state if others can contribute.",
-        name: "contribution",
-        default:'Contribution list'
-      },{//license
-        type: "list",
-        message: "Please select which license you would like to use?",
-        name: "License",
-        choices:["Academic Free License v3.0",
-        "Apache license 2.0",
-        "Artistic license 2.0",
-        "Boost Software License 1.0",
-        "BSD 2-clause license",
-        "BSD 3-clause license",
-        "Creative Commons Zero v1.0 Universal",
-        "Creative Commons Attribution 4.0",
-        "Creative Commons Attribution Share Alike 4.0",
-        "None"]
-      },
-      {// links
-        type: "input",
-        message: "Please give your GitHub profile link.",
-        name: "author",
-        default:'https://github.com/Morganbb104'
-
-      },{//Github
-        type: "input",
-        message: "what is your GitHub Account?",
-        name: "github",
-        default:'morganbb104'
-
-      },
-      {//Email
-        type: "input",
-        message: "Enter your email?",
-        name: "email",
-        default:'hcc780921@gmail.com'
-
-      },
-      {//test
-        type: "input",
-        message: "Please state any test(s) require.",
-        name: "test"
-      }];
-
-
-
-
-
-// function to initialize program
-function init() {
-    inquirer.prompt(questions)
-        .then(answer => {
-            // console.log(answer);
-            return generateMarkdown(answer);
-        })
-        .then(data => {
-            // console.log(data);
-            fs.writeFileSync("READMEgenerated.md", data);
-            console.log("README file created!");
+                this.addEmployee();
             })
-        .catch(err => { console.log(err) });
+    }
+
+    addEmployee() {
+        inquirer
+            .prompt({
+                type: 'list',
+                name: 'confirmAddEmp',
+                message: 'Would you like to add an Engineer, add a Intern, or are you finished adding employees?',
+                choices: ['Add Engineer', 'Add Intern', 'I am finished adding employees']
+            })
+            .then((data) => {
+                if (data.confirmAddEmp === 'Add Engineer') {
+                    this.addEngineer();
+                } else if (data.confirmAddEmp === 'Add Intern') {
+                    this.addIntern();
+                } else {
+                    this.writeFile()
+                }
+            })
+    }
+
+    addEngineer() {
+        inquirer
+            .prompt([
+                {
+                    type: 'text',
+                    name: 'name',
+                    message: 'What is your engineers name?'
+                },
+                {
+                    type: 'text',
+                    name: 'id',
+                    message: 'What is your engineers id?'
+                },
+                {
+                    type: 'text',
+                    name: 'email',
+                    message: 'What is your engineers email?'
+                },
+                {
+                    type: 'text',
+                    name: 'github',
+                    message: 'What is your engineers github username?'
+                }
+            ])
+            .then(({ name, id, email, github }) => {
+                this.employees.push(new Engineer(name, id, email, github));
+
+                this.addEmployee();
+            })
+    }
+    addIntern() {
+        inquirer
+            .prompt([
+                {
+                    type: 'text',
+                    name: 'name',
+                    message: 'What is your interns name?'
+                },
+                {
+                    type: 'text',
+                    name: 'id',
+                    message: 'What is your interns id?'
+                },
+                {
+                    type: 'text',
+                    name: 'email',
+                    message: 'What is your interns email?'
+                },
+                {
+                    type: 'text',
+                    name: 'school',
+                    message: 'What is your interns school?'
+                }
+            ])
+            .then(({ name, id, email, school }) => {
+                this.employees.push(new Intern(name, id, email, school));
+
+                this.addEmployee();
+            })
+    }
+
+    writeFile() {
+        fs.writeFile('./dist/index.html', generateHTML(this.employees), (err) => {
+            if (err) throw err;
+
+            console.log('The file is created');
+        });
+
+    }
+
+
 }
 
-// function call to initialize program
-init();
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.Title}
-
-## Description
-${data.Description}
-
-## User Story
-
-AS A developer
-I WANT a README generator
-SO THAT I can quickly create a professional README for a new project
-
-
-  
-## Table of Contents
-
-    User Story
-    1.   I a command-line application that accepts user input.
-    2.   I want to be prompted for information about my application repository.
-    3.   I want a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contribution Guidelines, Tests Instructions, and Questions.
-    4.   I want my project title to display as the title of the README.
-    5.   I want a description, installation instructions, usage information, contribution guidelines, and test instructions; and then, this information is added to the sections of the README entitled Description, Installation, Usage, Contribution Guidelines, and Tests Instructions.
-    6.   I want to choose a license for my application from a list of options; and then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under.  
-    7.   I want to enter my GitHub username; ant then this is added to the section of the README entitled Questions, with a link to my GitHub profile.
-    8.   I want to enter my email address; and then this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions.
-    9.   I want to click on the links in the Table of Contents; and then, I am taken to the corresponding section of the README.
- 
-## Usage
-   
-​   This application is used for quick readme generator ${data.use}
-
-## License
-   ${licenseBadge(data.License)}
-  
-## Installation
-   The following necessary dependencies must be installed to run the application properly: undefined
-   ${data.install}
-## Questions 
-   If you have any question please don't hesitate to contact me 
-   my email is ${data.email}
-   my Github name is ${data.github}, you can find me via this link ${data.author}
-
-## Contributors
-   ${data.contribution}
-
-## Test Instructions
-   ${data.test}
-
-`;
-}
-
-
-module.exports = generateMarkdown;
-
-
+const init = new Prompts();
+init.promptUser();
+module.exports = Prompts;
---
+++ rsc/template.js
@@ -0,0 +1,50 @@
+function generateHTML(data) {
+    let employeeInfo = ``;
+    for (var i = 0; i < data.length; i = i + 1) {
+        let extraItem = ``;
+        if(data[i].getRole() === 'Role: Manager'){
+            extraItem = data[i].getOfficeNumber()
+        }
+        else if(data[i].getRole() === 'Role: Intern'){
+            extraItem = data[i].getSchool()
+        }
+        else if(data[i].getRole() === 'Role: Engineer'){
+            extraItem = data[i].getGithub()
+        }
+
+        let card = `<div>
+            <h2>Name:${data[i].getName()}</h2>
+            <h3>Role: ${data[i].getRole()}</h3>
+            <p>ID:${data[i].getId()}</p>
+            <p>Email:<a href="mailto:https://${data[i].getEmail()}">${data[i].getEmail()}</a></p>
+            <p>${extraItem}</p>
+        </div>`;
+
+        employeeInfo += card;
+    }
+
+
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profiles</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header-wrapper">
+        <h1>Team Profiles</h1>
+    </div>
+    <div class = "grid">
+    ${employeeInfo}
+    </div>
+</body>
+</html>
+    `;
+
+}
+
+module.exports = generateHTML;
